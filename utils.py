2afd372eaf52f08cb693c001406401e6bee5ecd3
cylon <cylonn@yandex.ru>
2026-05-08 22:05:20 +0500

    changed get_favorites_ids

--- utils.py
+++ utils.py
@@ -1,16 +1,14 @@
-from typing import List
-
-from database.models import Favorite
+from database.service import get_favorites
 
 
-def get_favorites_ids(list_of_favorites: List[Favorite]):
-    """Возвращает список VK ID из записей избранного.
+def get_favorites_ids(current_user_id):
+    """Возвращает список VK ID избранных профилей пользователя.
 
     Args:
-        list_of_favorites: список объектов Favorite.
+        current_user_id: VK ID текущего пользователя.
 
     Returns:
-        Список строк profile_vk_id в том же порядке.
+        Список строк profile_vk_id в том же порядке, что хранятся в БД.
     """
+    list_of_favorites = get_favorites(current_user_id)
     return [favorite.profile_vk_id for favorite in list_of_favorites]
-
